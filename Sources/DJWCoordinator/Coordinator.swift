c8daf3dd552f7fd183fd9117cf9f5d4e3304a76c
Trevor <trevor.d@beydag.com>
2020-01-29 23:56:00 +0000

    First push

---
+++ Sources/DJWCoordinator/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  MVVM-C
+//
+//  Created by dejaWorks on 29/01/2020.
+//  Copyright © 2020 dejaWorks. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator: class{    
+    func start()
+}
+
